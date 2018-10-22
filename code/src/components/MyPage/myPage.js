ecbf27b332a7cf5aeb65459665a1d0111f94658c
Jeanette <aejlindberg@gmail.com>
2018-10-22 17:43:46 +0200

    Added grid to myPage

--- code/src/components/MyPage/myPage.js
+++ code/src/components/MyPage/myPage.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom"
 import "./myPage.scss"
-
+import ActivityGrid from "../ActivityGrid/activityGrid.js"
 class MyPage extends React.Component {
 
 state = {
@@ -21,31 +21,25 @@ componentDidMount() {
   this.getChosenTeam()
 }
 
-  render() {
-    return (
-      <div className="wrapper">
-        <div className="mp-header-section">
-          <h1>Min sida</h1>
-          <Link to="/">
-            <button className="mp-header-section-button">Byt förening</button>
-          </Link>
-        </div>
-        <div className="activity-section-grid">
-          A-Games Grid
-        </div>
-        <div className="activity-popup">
-          <div className="activity-popup-inner">
-            <h2>Aktivitet</h2>
-            <button className="activity-popup-button">X</button>
-          </div>
-        </div>
-        <div className="activity-section-log">
-        LOG
-        </div>
-        <h1>MITT LAG: {this.state.myTeam}</h1>
+render() {
+  return (
+    <div className="wrapper">
+      <div className="mp-header-section">
+        <h1>Min sida</h1>
+        <Link to="/">
+          <button className="mp-header-section-button">Byt förening</button>
+        </Link>
       </div>
-    )
-  }
+      <div className="activity-section-grid">
+        <ActivityGrid />
+      </div>
+      <div className="activity-popup">
+      Popup
+      </div>
+      <h1>MITT LAG: {this.state.myTeam}</h1>
+    </div>
+  )
+}
 
 }
 
